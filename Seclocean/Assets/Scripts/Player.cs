cf7be28c34fa9bf1882ba7026ccdc26d95ec8057
Gavin Sekhon <gggavin36@gmail.com>
2018-10-09 21:41:36 -0700

    moving angie

---
+++ Seclocean/Assets/Scripts/Player.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour {
+
+    Animator animator;
+    public float speedValue;
+    // Use this for initialization
+    void Start () {
+        animator = GetComponent<Animator>();
+    }
+    
+    // Update is called once per frame
+    void Update () {
+        float speed = Input.GetAxis("Horizontal") * Time.deltaTime * speedValue;
+
+        transform.localScale = new Vector3(speed / Mathf.Abs(speed), 1, 1);
+
+        animator.SetFloat("speed", Mathf.Abs(speed));
+
+        transform.Translate(new Vector3(speed, 0));
+    }
+}
