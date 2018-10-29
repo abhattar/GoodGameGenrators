0cfd3f33ab88939e08ee9da781a2bad3222db9c9
Gavin Sekhon <gggavin36@gmail.com>
2018-10-28 18:34:00 -0700

    Home World - Add Thing

--- Seclocean/Assets/Scripts/Player.cs
+++ Seclocean/Assets/Scripts/Player.cs
@@ -6,27 +6,86 @@ public class Player : MonoBehaviour {
 
     Animator animator;
     public float speedValue;
+    public Camera sideCamera;
+    public Camera overheadCamera;
+
+    private float initSpeedValue;
+
+
     // Use this for initialization
     void Start () {
         animator = GetComponent<Animator>();
+        initSpeedValue = speedValue;
     }
     
     // Update is called once per frame
     void Update () {
+
+        if (Input.GetKeyDown(KeyCode.J))
+        {
+            speedValue = initSpeedValue * 2;   
+        }
+        else if (Input.GetKeyUp(KeyCode.J)){
+            speedValue = initSpeedValue;
+        }
+
         float speed = Input.GetAxis("Horizontal") * Time.deltaTime * speedValue;
         float speed2 = Input.GetAxis("Vertical") * Time.deltaTime * speedValue;
 
-        transform.localScale = new Vector3(speed / Mathf.Abs(speed), 1, 1);
+        
+        
 
-        animator.SetFloat("speed", Mathf.Abs(speed));
-        animator.SetFloat("speed2", Mathf.Abs(speed));
+        // transform.localScale = new Vector3(speed / Mathf.Abs(speed), 1, 1);
+
+        // animator.SetFloat("speed", Mathf.Abs(speed));
+        // animator.SetFloat("speed2", Mathf.Abs(speed));
 
         transform.Translate(new Vector3(speed, speed2));
 
-        if (Input.GetKeyDown(KeyCode.Space))
+  
+      
+
+        if (Input.GetKeyDown(KeyCode.K))
         {
             GetComponent<Animator>().SetTrigger("Bite");
         }
+
+        Vector3 pos = transform.position;
+
+        if (Input.GetKeyDown(KeyCode.W)){
+            pos.y += speed * Time.deltaTime;
+        }
+        if(Input.GetKeyDown(KeyCode.S)){
+            pos.y -= speed * Time.deltaTime;
+
+        }
+        if(Input.GetKeyDown(KeyCode.A)){
+            pos.x -= speed * Time.deltaTime;
+
+        }
+        if(Input.GetKeyDown(KeyCode.D)){
+            pos.x += speed * Time.deltaTime;
+        }
+
+       
+
+
+
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            if(sideCamera){
+                overheadCamera.enabled=true;
+                sideCamera.enabled=false;
+            }
+            else{
+                overheadCamera.enabled=false;
+                sideCamera.enabled=true;
+            }
+        }
+
+   
+
     }
 
     void OnTriggerEnter2D(Collider2D col)
@@ -36,6 +95,7 @@ public class Player : MonoBehaviour {
                 if (GetComponent<Animator>().GetCurrentAnimatorStateInfo(0).IsName("Angie Bite")) { 
 				    Destroy(col.gameObject);
                 }
+                
             
             }	   
     }
